085c08e2e18354461c9e5b4905a5123d9d68622b
zorins <72649404+zorinss@users.noreply.github.com>
2026-07-17 00:21:26 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gymplan-v3-red';
+const CACHE = 'gymplan-v3-mockup';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
